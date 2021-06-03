313a5ed95e075f3b8c11645751202b3e61965e20
Won Yang <won@gatech.edu>
2021-06-03 12:35:40 -0500

    adds python solution

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,24 @@
-def binary_search():
-    pass
+def binary_search(num_to_search, array):
+    sorted_array = sorted(array)
+
+    # sets initial index values
+    low = 0
+    high = len(sorted_array) - 1
+
+    while True:
+        # we know num's not found if low becomes greater than high. This is b/c the elif branches will eventually become low > high if num isn't found.
+        if low > high:
+            break
+
+        # sets the mid value based on high and low
+        mid = low + ((high - low) // 2)
+
+        # make comparisons to middle num
+        if num_to_search == sorted_array[mid]:
+            return mid
+        elif num_to_search < sorted_array[mid]:
+            high = mid - 1
+        elif num_to_search > sorted_array[mid]:
+            low = mid + 1
+
+    return -1  # if not found
--- python/binary_search_spec.py
+++ python/binary_search_spec.py
@@ -1,16 +1,37 @@
-# Write your unit tests here
-
+import unittest
 from binary_search import binary_search
 
-test_array = [1,2,3,4,5]
+test_array = [1, 2, 3, 4, 5]
+super_big_array = [1, 5, 7, 2, 3, 8, 4, 9]
 
+
+class TestBinarySearch(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(binary_search(1, test_array), 0)
+
+    def test_case_2(self):
+        self.assertEqual(binary_search(3, test_array), 2)
+
+    def test_case_3(self):
+        self.assertEqual(binary_search(5, test_array), 4)
+
+    def test_case_4(self):
+        self.assertEqual(binary_search(6, super_big_array), -1)
+
+    def test_case_5(self):
+        self.assertEqual(binary_search(7, super_big_array), 5)
+
+
+"""
 print(binary_search(1, test_array) == 0)
 print(binary_search(2, test_array) == 1)
 print(binary_search(3, test_array) == 2)
 print(binary_search(4, test_array) == 3)
 print(binary_search(5, test_array) == 4)
 
-
-super_big_array = [1,5,7,2,3,8,4,9]
 print(binary_search(7, super_big_array) == 5)
 print(binary_search(6, super_big_array) == -1)
+"""
+
+if __name__ == "__main__":
+    unittest.main()
